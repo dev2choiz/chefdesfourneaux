a97bee9fbba6ee3cff98ced14169a7e9c59bc4ee
NailaMelhem <naila.melhem@gmail.com>
2015-03-04 10:14:44 +0100

    Produit insert

--- Application/Controllers/Vente.php
+++ Application/Controllers/Vente.php
@@ -4,13 +4,14 @@ namespace Application\Controllers;
 
 class Vente extends \Library\Controller\Controller{
 
-	
+	private $modelProduits;
 
 
 	public function __construct(){
 
 		$this->setLayout("carousel");
-		//$this->setLayout("blog");
+		
+		$this->modelProduits = new \Application\Models\Produit('localhost');
 		
 	}
 
@@ -28,6 +29,52 @@ class Vente extends \Library\Controller\Controller{
 			"ajax" => $ajax
 			));
 
+
+	}
+
+	public function indexRestaurantAction(){
+		
+
+
+		$modelAjax 	= new \Application\Models\Ajax('localhost');
+		
+		$ajax= $modelAjax->getAjaxPost("viewrecette", "getallviewrecettes", array(), "console.log(data);"  );
+
+		$this->setDataView(array(
+			"pageTitle" => "Catégories de recettes, cuisine du monde, recettes authentique, santé, cuisine légère",
+			"ajax" => $ajax
+			));
+
+
+	}
+
+	public function indexProduitAction(){
+		$produits = $this->modelProduits->getAllProduits();
+		//var_dump($produits);
+
+		$ajax = "coucou";
+		if(isset($_POST["btnProduit"])){
+			unset($_POST["btnProduit"]);
+			$this->modelProduits->insertProduit($_POST);
+		}
+		
+		$this->setDataView(array(
+			'pageTitle' => "Vente d'ustensile de cuisine, vente d'électroménager semi-pro",
+			'produits' => $produits['response'],
+			'ajax' => $ajax ));
+
+	}
+
+	public function produitAction(){
+		$produits = $this->modelProduits->getAllProduits();
+		//var_dump($produits);
+
+		$ajax = "coucou";
+
+		$this->setDataView(array(
+			'pageTitle' => "Vente d'ustensile de cuisine, vente d'électroménager semi-pro",
+			'produits' => $produits['response'],
+			'ajax' => $ajax ));
 
 	}
 
