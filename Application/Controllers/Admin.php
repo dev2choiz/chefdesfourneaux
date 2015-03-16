86f00987811ff6a2cf57576c5eacbc242252253d
Dev2Choiz <dev2choiz@gmail.com>
2015-03-16 16:13:19 +0100

    mettre aussi a jour les images des recettes

--- Application/Controllers/Admin.php
+++ Application/Controllers/Admin.php
@@ -262,16 +262,18 @@ class Admin extends \Library\Controller\Controller{
 			
 			$modelRecette 	= new \Application\Models\Recette('localhost');
 
-
-
+				echo "<br><br><br><br><br><br><br><br><br><br>";
+			var_dump($_FILES, $_POST);
 			//######################## Copie et met a jour dans la base
 	        $root = $_FILES['img']['tmp_name'];
 	        $img = IMG_ROOT.$this->retirerCaractereSpeciaux($_POST["titre"].".jpg");
 
+
+	        	var_dump($root, $img);
 	        if(copy($root, $img )){
-	        	
 	        	$_POST['img'] = '/img/'.$this->retirerCaractereSpeciaux($_POST["titre"]).".jpg";
 	        }else{
+	        	$_POST['img'] ="";
 	        	$this->message->addError("Pb avec la mise a jour de l'image");
 	        }
 
