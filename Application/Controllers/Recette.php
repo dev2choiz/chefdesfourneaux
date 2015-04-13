a8758d91507c97bcadc44c6eed19f8586e401bf1
Dev2Choiz <dev2choiz@gmail.com>
2015-04-14 01:49:39 +0200

    barre de recherche sync 2

--- Application/Controllers/Recette.php
+++ Application/Controllers/Recette.php
@@ -153,24 +153,22 @@ class Recette extends \Library\Controller\Controller{
 		$resultRecherche  	= $this->convEnTab($this->modelViewRecette->getRecherche($search, 'titre') );
 		$resultRecherche 	= $resultRecherche['response'];
 		echo "<br><br><br><br><br><br><br><br><br>";
-
 		if (!empty($resultRecherche)) {
 				//2 cas:
 				//		1 resulat
 				if (count($resultRecherche)==1 ) {
-					var_dump($resultRecherche);
+					//var_dump($resultRecherche);
 					//on redirige ver la page qui affiche la recette
-					header('location: '.LINK_ROOT.'/recette/categorie/'.$resultRecherche[0]['id_recette']);
+					header('location: '.LINK_ROOT.'recette/categorie/'.$resultRecherche[0]['id_recette']);
 
-				} else {	//	>1 
-					//on dirige vers une page qui affiche la liste des 
-					//recettes
-					//selectionnées
+				} /*else {	//	nbrResult>1 
+					//on affiche la liste des 
+					//recettes selectionnées
 					
 				}
 				
 				//		 
-		} else {
+		} else {*/
 			
 		}
 		
@@ -178,7 +176,7 @@ class Recette extends \Library\Controller\Controller{
 		$this->setDataView(array(
 			"pageTitle" 		=> "Recettes ",
 			"message" 			=> $this->message->showMessages(),
-			"titreCat"			=> $titreCat
+			"recettes"			=> $resultRecherche
 		));
 	}
 
