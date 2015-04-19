6dbfa65c4f9d1d44f291d64431b996a28f88968a
Dev2Choiz <dev2choiz@gmail.com>
2015-04-19 21:59:20 +0200

    sync

--- Public/js/usermotDePasseOublie.js
+++ Public/js/usermotDePasseOublie.js
@@ -5,23 +5,19 @@ $(document).ready(function(){
         $('#id_questionsecrete').hide();
         $( "#id_questionsecrete" ).prop( "disabled", true );
 
-        alert("dkj");
+        //alert("dkj");
         $('#mail').blur(function(){
             
             mail=$('#mail').val();
-
             
             if (true /*Verif format mail ici*/) {
-
-                alert(mail);
                 idQuestUser=questionUser(mail);
-                console.log("idq",idQuestUser);
+                
                 if(idQuestUser>0 ){
 
                     $('#id_questionsecrete option[value="'+idQuestUser+'"]').prop('selected', true);
                     $("#id_questionsecrete").show('slow');
-
-                    //chargerQuestions(mail, idQuestUser);
+                    
                 }else{
                     alert("le mail n'existe pas dans la base");
                 }
@@ -42,7 +38,6 @@ $(document).ready(function(){
 
 
 function questionUser(mail){
-    //
 
     jsonData = 
     {
@@ -50,7 +45,6 @@ function questionUser(mail){
         'method'        : 'getQuestionSecreteUser',
         'mail'        : mail
     };
-
 
     var retour=null;
     $.ajax({
@@ -67,41 +61,4 @@ function questionUser(mail){
 
     return retour;
 }
-
-function chargerQuestions(mail, idQuestUser){
-
-
-    jsonData = 
-    {
-        'service'       : 'QuestionSecrete',
-        'method'        : 'getQuestionSecretes',
-        'id_com'        : mail
-    };
-
-
-
-    $.ajax({
-        type: 'POST',
-        data: jsonData,
-        url: urlWebService,
-        dataType: 'json',
-        async:false,
-        success: function(data) {
-            console.log("les question",data.response);
-            $("#id_questionsecrete").empty();
-            for (var i = 0; i < data.response.length; i++) {
-                $("#id_questionsecrete").append("<option value='"+data.response[i]['id_questionsecrete']+"'>"+data.response[i]['value']+"</option>");
-            };
-
-            $('#id_questionsecrete option[value="'+idQuestUser+'"]').prop('selected', true);
-            $("#id_questionsecrete").show('slow');
-            
-            
-
-        }
-    });
-
-    return true;
-}
-
 
